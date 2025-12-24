77166aed5dc666a3550d2206a368d6302056ca97
123456 <hoochanlon@outlook.com>
2025-12-25 04:29:11 +0800

    asd

--- api/file.js
+++ api/file.js
@@ -1,4 +1,24 @@
 export default async function handler(req, res) {
+  // 设置 CORS 头，允许跨域请求
+  const origin = req.headers.origin;
+  const allowedOrigins = [
+    'https://hoochanlon.github.io',
+    'https://blog.hoochanlon.moe',
+    'https://picx-images-hosting-brown.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:8000'
+  ];
+  
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
