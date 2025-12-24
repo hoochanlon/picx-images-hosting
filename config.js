750f7457914c037b32f26720a006df29c5ed651b
123456 <hoochanlon@outlook.com>
2025-12-25 04:23:07 +0800

    asd

---
+++ config.js
@@ -0,0 +1,16 @@
+// 配置文件
+// 可以根据不同环境修改这些值
+
+window.APP_CONFIG = {
+  // Vercel API 基础地址
+  VERCEL_API_BASE: 'https://picx-images-hosting-brown.vercel.app',
+  
+  // 自定义域名列表（需要使用 Vercel API 的域名）
+  CUSTOM_DOMAINS: [
+    'blog.hoochanlon.moe'
+  ],
+  
+  // GitHub Pages 域名模式（匹配所有 github.io 域名）
+  GITHUB_PAGES_PATTERN: /\.github\.io$/
+};
+
